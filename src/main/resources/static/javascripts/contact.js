e6fe3bdbd1e75d59acfdfc3297f842a42f3a1e56
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-03-15 01:19:37 +0700

    Update

--- src/main/resources/static/javascripts/contact.js
+++ src/main/resources/static/javascripts/contact.js
@@ -1,36 +1,33 @@
-function h3alignment() {
-    let namePhone = document.getElementById("namePhone");
-    let phoneInput = document.getElementById("phoneInput");
-    let npWidth = namePhone.offsetWidth;
-    let inputWidth = phoneInput.offsetWidth;
-    let paddingNum = (npWidth - (inputWidth * 2)) / 2;
-    let h3Element = document.getElementById("h3Element");
-    if (window.innerWidth > 500) {
-        h3Element.style.paddingLeft = paddingNum + "px";
-    } else {
-        h3Element.style.paddingLeft = "0px";
-    }
-}
+
 
 
 var inputList = document.querySelectorAll('.otp-cells input')
-var inputedOtp="";
 
-inputList.forEach((input) => {
-    input.addEventListener('input', function () {
-        if (this.value.length > this.maxLength) {
-            this.value = this.value.slice(0, 1);
-        } else if (this.value.length === this.maxLength) {
-            inputedOtp+=this.value;
-            let nextCell = this.nextElementSibling;
-            if (nextCell) nextCell.focus();
-        }
-        if ([...inputList].every(inp => inp.value.length === 1)) {
-            [...inputList].every(inp => inp.disabled = true)
-            document.querySelector(".loader").style.display = "block";
-        }
+function otpTyping() {
+    var inputedOtp="";
+    inputList.forEach((input) => {
+        input.addEventListener('input', function () {
+            if (this.value.length > this.maxLength) {
+                this.value = this.value.slice(0, 1);
+            } else if (this.value.length === this.maxLength) {
+                inputedOtp+=this.value;
+                console.log("max length: "+ this.maxLength)
+                console.log("length: "+ this.value.length)
+                console.log("inputed otp: "+ inputedOtp)
+                let nextCell = this.nextElementSibling;
+                if (nextCell) nextCell.focus();
+            }
+            if ([...inputList].every(inp => inp.value.length === 1)) {
+                [...inputList].every(inp => inp.disabled = true)
+                document.querySelector(".loader").style.display = "block";
+            }
+        });
+    console.log(inputedOtp);
     });
-});
+
+}
+
+
 
 document.querySelectorAll(".contactInfo input, .contactInfo textarea").forEach((element) => {
     element.addEventListener("input", () => {
@@ -40,6 +37,8 @@ document.querySelectorAll(".contactInfo input, .contactInfo textarea").forEach((
         document.querySelector(".contactInfo button").disabled = !allInputsFilled;
     });
 });
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("sendBtn").addEventListener("click", function () {
@@ -59,10 +58,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-
-
-h3alignment();
-
-window.addEventListener("DOMContentLoaded", h3alignment);
-window.addEventListener("resize", h3alignment);
